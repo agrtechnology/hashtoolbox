539f712bcd0ad5d0a8e95f8c5c7a146b37265dc6
Aquae <daniel.aquadd@gmail.com>
2017-08-23 19:48:09 +1000

    Bug fixes & overall improvement
    Fixed sum bugs (hopefully doesn't make more) & made moar dynamic.

--- main.py
+++ main.py
@@ -1,33 +1,37 @@
-import hashlib
+import tkinter.filedialog
 import tkinter as tk
-
-class HashResult(tk.Frame):
-    def __init__(self, parent):
-        super().__init__(parent)
-
-        tk.Label(self, text="MD5").grid(row=0, column=0)
-        tk.Label(self, text="SHA1").grid(row=1, column=0)
-        tk.Label(self, text="SHA256").grid(row=2, column=0)
-
-        self.hash_results = (tk.Entry(self), tk.Entry(self), tk.Entry(self))
-        for i in range(len(self.hash_results)):
-            self.hash_results[i].grid(row=i, column=1)
+import hashlib
 
 class FileSelector(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
 
-        self.select_file_btn = tk.Button(self, text="Select File", command=parent._create_hashes)
+        self.select_file_btn = tk.Button(self, text="Select File", command=parent._generate_hashes)
         self.select_file_lbl = tk.Label(self, text="No File Selected")
         self.select_file_btn.grid(row=0, column=0)
         self.select_file_lbl.grid(row=0, column=1)
 
+class HashResult(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.hash_results = []
+
+        for i in range(len(parent.hash_functions)):
+            tk.Label(self, text=parent.hash_functions[i]).grid(row=i, column=0)
+            self.hash_results.append(tk.Entry(self))
+            self.hash_results[i].grid(row=i, column=1)
+
 class App(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
-        parent.title("HashToolBox v1.0")
-        
-        self.filename = None
+        parent.title("HashToolBox v1.1")
+
+        self.hash_functions = (
+            "MD5",
+            "SHA1",
+            "SHA256"
+            )
 
         self.file_selector = FileSelector(self)
         self.file_selector.grid(row=0, column=0, sticky=tk.W)
@@ -35,35 +39,24 @@ class App(tk.Frame):
         self.hashes = HashResult(self)
         self.hashes.grid(row=1, column=0, rowspan=2)
 
-        self.bind("<Configure>", self._triggered)
-
-    def _triggered(self, event):
-        print(event)
-
-    def _create_hashes(self):
-        self._get_file()
-        self._generate_hashes()
-
-    def _get_file(self):
-        self.filename = tk.filedialog.askopenfile().name
-        self.file_selector.select_file_lbl.config(text=self.filename)
-
     def _generate_hashes(self):
-        md5 = hashlib.md5()
-        sha1 = hashlib.sha1()
-        sha256 = hashlib.sha256()
-        with open(self.filename, "rb") as f:
-            buf = f.read()
-            md5.update(buf)
-            sha1.update(buf)
-            sha256.update(buf)
+        file = tk.filedialog.askopenfile()
+        if file:
+            filename = file.name
+            self.file_selector.select_file_lbl.config(text=filename)
+            
+            with open(filename, "rb") as f:
+                buf = f.read()
 
-        hashes = (md5.hexdigest(), sha1.hexdigest(), sha256.hexdigest())
-        for i in range(len(hashes)):
-            self.hashes.hash_results[i].delete(0, tk.END)
-            self.hashes.hash_results[i].insert(0, hashes[i])
+            for i in range(len(self.hash_functions)):
+                hasher = hashlib.new(self.hash_functions[i])
+                hasher.update(buf)
+                hash_result = hasher.hexdigest()
+                
+                self.hashes.hash_results[i].delete(0, tk.END)
+                self.hashes.hash_results[i].insert(0, hash_result)
 
 if __name__ == "__main__":
     root = tk.Tk()
-    App(root).pack()
+    App(root).grid()
     root.mainloop()
